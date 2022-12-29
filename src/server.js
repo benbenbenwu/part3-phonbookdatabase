54725df2afbc85fedc2107140eeab770eff2fce7
ben <ben@example.com>
2022-12-29 19:55:14 +0800

    v1

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-const serverless = require('serverless-http')
 const Person = require('./models/person')
 const errorHandler = require('./utils/errorHandler')
 
@@ -106,17 +105,11 @@ router.post('/persons', async (req, res, next) => {
 })
 
 
-app.use('/.netlify/functions/api', router)
 
-//app.use('/api', router)
-
-
+app.use('/api', router)
 
 app.use(errorHandler)
 
-/* app.listen(process.env.PORT || '3001', () => {
+app.listen(process.env.PORT || '3001', () => {
   console.log('Listening port 3001......');
-}) */
-module.exports = app;
-
-module.exports.handler = serverless(app) 
+}) 
